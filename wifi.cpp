7f8646f9697ae539a2c79e94730708ef4c222ff8
Vince (Atlas) <git@darkain.com>
2018-06-22 19:40:39 -0700

    Removing old wifi pinging code

--- wifi.cpp
+++ wifi.cpp
@@ -9,30 +9,8 @@
 
 
 
-WiFiUDP				_udpSender;
-IPAddress			_broadcastIp;
-int					_broadcastPort		= 0;
-long				_lastHeartbeat		= 0;
-struct netif*		_ESPif				= NULL;
-netif_input_fn		_originalInputFn	= NULL;
-long				_lastNetifInput		= 0;
 wl_status_t			wifi_status			= WL_IDLE_STATUS;
 
-
-
-extern "C" {
-	struct netif* eagle_lwip_getif(uint8_t index);
-	#include "netif/etharp.h"
-}
-
-
-
-
-err_t netif_input_local(struct pbuf* p, struct netif* inp) {
-	_lastNetifInput = millis();
-//	printPacketInfo(p);
-	return _originalInputFn( p, inp );
-}
 
 
 
@@ -61,139 +39,8 @@ static String softAPPSK() {
 }
 
 
-/*
-int MACsprintf( const uint8_t* MAC, char* buffer, size_t bufferLength ) {
-	return snprintf( buffer, bufferLength, "%02X:%02X:%02X:%02X:%02X:%02X",
-		MAC[0], MAC[1], MAC[2], MAC[3], MAC[4], MAC[5]);
-}
-*/
-
-
-/*
-int IPsprintf( const uint8_t* IP, char* buffer, size_t bufferLength ) {
-	return snprintf( buffer, bufferLength, "%d.%d.%d.%d", IP[0], IP[1], IP[2], IP[3]);
-}
-*/
-
-
-/*
-void printPacketInfo(struct pbuf* p) {
-	struct EtherFrame* FrameHeader = (struct EtherFrame*) p->payload;
-	uint16_t type = swap16(FrameHeader->typeLength);
-	uint8_t* data  = (uint8_t*)(p->payload);
-	char text[128];
-
-
-	if (type == 0x0806) {
-//		printARP((uint8_t*)(p->payload));
-//		Serial.println("");
-
-	} else if (type == 0x0800) {
-//		printIP((uint8_t*)(p->payload));
-//		Serial.println("");
-
-	} else {
-		char srcMAC[ 32 ];
-		char destMAC[ 32 ];
-		MACsprintf( FrameHeader->srcMAC, srcMAC, sizeof( srcMAC ) );
-		MACsprintf( FrameHeader->destMAC, destMAC, sizeof( destMAC ) );
-
-		snprintf(text, sizeof(text), "Source: %s Dest: %s Type: %04X", srcMAC, destMAC, type );
-		Serial.println(text);
-		Serial.println("");
-	}
-}
-*/
-
-
-/*
-void printARP(uint8_t* data) {
-	char text[256];
-	char srcMAC[32];
-	char destMAC[32];
-	char srcIP[32];
-	char destIP[32];
-	// uint16_t type = 0;
-
-	struct etharp_packet* arp = (struct etharp_packet*)data;
-	uint16_t opcode = swap16(arp->opcode);
-
-	// filter - ONLY ME!!!
-	//if (arp->dst_ip_addr[3] != 142 && arp->src_ip_addr[3] != 142 ) return;
-	//if (arp->dst_ip_addr[3] == 1 || arp->src_ip_addr[3] == 1 ) return; // from/to the router
-
-	IPsprintf(arp->src_ip_addr, srcIP, sizeof(srcIP) );
-	IPsprintf(arp->dst_ip_addr, destIP, sizeof(destIP) );
-	if (opcode == 1) snprintf(text, sizeof(text), "ARP => %s, Request", srcIP);
-	else if (opcode == 2) snprintf(text, sizeof(text), "ARP <= %s, Reply", destIP);
-	else snprintf(text, sizeof(text), ": ARP -----");
-	Serial.println(text);
-
-	MACsprintf(arp->hdr.srcMAC, srcMAC, sizeof(srcMAC));
-	MACsprintf(arp->hdr.destMAC, destMAC, sizeof(destMAC));
-	snprintf(text, sizeof(text), "ETH: Source: %s Dest: %s", srcMAC, destMAC);
-	Serial.println(text);
-
-	MACsprintf(arp->src_eth_addr, srcMAC, sizeof(srcMAC));
-	MACsprintf(arp->dst_eth_addr, destMAC, sizeof(destMAC));
-	snprintf(text, sizeof(text), "MAC: Source: %s Dest: %s", srcMAC, destMAC);
-	Serial.println(text);
-
-	snprintf(text, sizeof(text), "IP: Source: %s Dest: %s Opcode: %04X", srcIP, destIP, opcode );
-	Serial.println(text);
-}
-*/
-
-
-/*
-void printIP(uint8_t* data) {
-	char text[256];
-	char srcMAC[32];
-	char destMAC[32];
-	char srcIP[32];
-	char destIP[32];
-	// uint16_t type = 0;
-
-	struct udp_packet* udp = (struct udp_packet*)data;
-	IPsprintf(udp->ip.ip_src_addr, srcIP, sizeof(srcIP));
-	IPsprintf(udp->ip.ip_dst_addr, destIP, sizeof(destIP));
-
-	snprintf(text, sizeof(text), "IP/%d %s ==> %s: port=%d", udp->ip.ip_proto, srcIP, destIP, swap16(udp->dst_port));
-	Serial.println(text);
-}
-*/
-
-
-/*
-void printMyMac() {
-	byte mac[6];
-	WiFi.macAddress(mac);
-	Serial.print(mac[0], HEX);
-	Serial.print(":");
-	Serial.print(mac[1], HEX);
-	Serial.print(":");
-	Serial.print(mac[2], HEX);
-	Serial.print(":");
-	Serial.print(mac[3], HEX);
-	Serial.print(":");
-	Serial.print(mac[4], HEX);
-	Serial.print(":");
-	Serial.print(mac[5], HEX);
-}
-*/
-
-
 
 void wifi_setup() {
-	_broadcastIp = ~WiFi.subnetMask() | WiFi.gatewayIP();
-	_udpSender.begin(10010);
-
-	if((_ESPif = eagle_lwip_getif(0)) != NULL) {
-		_originalInputFn = _ESPif->input;
-		_ESPif->input = netif_input_local;
-	}
-
-	_lastNetifInput = millis();
 }
 
 
@@ -240,6 +87,42 @@ WL_CONNECTION_LOST  = 5,
 WL_DISCONNECTED     = 6
 */
 
+
+//TODO: DO SOMETHING HERE
+void pingFault (void) {}
+
+
+
+void wifi_connected() {
+	startPingAlive();
+	//TODO: FIND A WAY TO TURN PING ALIVE OFF ONCE CONNECTION IS LOST
+
+	Serial.println(F("\nWifi connection established"));
+
+	Serial.print(F("\tNetwork:\t"));
+	Serial.println(WiFi.SSID());
+
+	Serial.print(F("\tIP address:\t"));
+	Serial.println(WiFi.localIP());
+
+	Serial.print(F("\tSubnet mask:\t"));
+	Serial.println(WiFi.subnetMask());
+
+	Serial.print(F("\tGateway:\t"));
+	Serial.println(WiFi.gatewayIP());
+
+	Serial.print(F("\tDNS:\t\t"));
+	Serial.println(WiFi.dnsIP());
+
+	Serial.print(F("\tHostname:\t"));
+	Serial.println(WiFi.hostname());
+
+	Serial.println(F(""));
+}
+
+
+
+
 void wifi_loop() {
 	wl_status_t status = WiFi.status();
 	if (unlikely(status != wifi_status)) {
@@ -247,44 +130,8 @@ void wifi_loop() {
 		wifi_display();
 
 		if (wifi_status == WL_CONNECTED) {
-			Serial.println(F("\nWifi connection established"));
-
-			Serial.print(F("\tNetwork:\t"));
-			Serial.println(WiFi.SSID());
-
-			Serial.print(F("\tIP address:\t"));
-			Serial.println(WiFi.localIP());
-
-			Serial.print(F("\tSubnet mask:\t"));
-			Serial.println(WiFi.subnetMask());
-
-			Serial.print(F("\tGateway:\t"));
-			Serial.println(WiFi.gatewayIP());
-
-			Serial.print(F("\tDNS:\t\t"));
-			Serial.println(WiFi.dnsIP());
-
-			Serial.print(F("\tHostname:\t"));
-			Serial.println(WiFi.hostname());
-
-			Serial.println(F(""));
+			wifi_connected();
 		}
-	}
-
-
-	if (likely(_broadcastPort > 0)  &&  unlikely(millis() - _lastHeartbeat >= 500)) {
-		_lastHeartbeat = millis();
-		_udpSender.beginPacket(_broadcastIp, _broadcastPort);
-		_udpSender.write("foo");
-		_udpSender.endPacket();
-	}
-
-	if (unlikely(millis() - _lastNetifInput >= 10000)) {
-		_lastNetifInput = millis();
-		char buf[100] = "";
-		snprintf(buf, 100, FS("Unresponsive, resetting... (uptime %d)"), millis() / 1000);
-		//Serial.println(buf);
-		//ESP.restart();
 	}
 }
 
--- wifi.h
+++ wifi.h
@@ -5,90 +5,15 @@
 
 #include <ESP8266WiFi.h>
 #include <lwip/netif.h>
+#include "src/pingalive/src/PingAlive.h"
 
 
 
 void wifi_setup();
 void wifi_loop();
+void wifi_display();
+void wifi_connected();
 
 
-
-extern int _broadcastPort;
-
-
-
-
-struct EtherFrame {
-	uint8_t destMAC[6];
-	uint8_t srcMAC[6];
-	uint16_t typeLength;
-} __attribute__((packed));
-
-
-
-
-struct etharp_packet {
-	struct EtherFrame hdr;
-	uint16_t hwtype;
-	uint16_t proto;
-	uint8_t  hwlen;
-	uint8_t  protolen;
-	uint16_t opcode;
-	uint8_t  src_eth_addr[6];
-	uint8_t  src_ip_addr[4];
-	uint8_t  dst_eth_addr[6];
-	uint8_t  dst_ip_addr[4];
-} __attribute__((packed));
-
-
-
-
-struct IP_Frame {
-	// #if BYTE_ORDER == LITTLE_ENDIAN
-	uint8_t   ip_hlen:4;  // header length
-	uint8_t   ip_ver:4;   // version
-
-	uint8_t   ip_tos;     // type of service
-	uint16_t  ip_len;     // total length
-	uint16_t  ip_id;      // identification
-	uint16_t  ip_off;     // fragment offset field
-	uint8_t   ip_ttl;     // time to live
-	uint8_t   ip_proto;   // protocol
-	uint16_t  ip_sum;     // checksum
-	uint8_t   ip_src_addr[4];  // destination address
-	uint8_t   ip_dst_addr[4];  // source address
-} __attribute__((packed));
-
-
-
-
-struct udp_packet {
-	struct EtherFrame hdr;
-	struct IP_Frame ip;
-	uint16_t  src_port;
-	uint16_t  dst_port;
-	uint16_t length;
-	uint16_t checksum;
-} __attribute__((packed));
-
-
-
-
-inline uint16_t swap16(uint16_t n) {
-	return ((n>>8)&0xff) | ((n<<8)&0xff00);
-}
-
-
-
-
-err_t netif_input_local(struct pbuf* p, struct netif* inp);
-/*
-int MACsprintf(const uint8_t* MAC, char* buffer, size_t bufferLength);
-int IPsprintf(const uint8_t* IP, char* buffer, size_t bufferLength);
-void printPacketInfo(struct pbuf* p);
-void printARP(uint8_t* data);
-void printIP(uint8_t* data);
-void printMyMac();
-*/
 
 #endif //__WIFI_H__
